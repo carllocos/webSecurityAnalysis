33af6cae6cc7d9fa67b967baafe1a4eab783a44a
carllocos <carlos.javier.montero.castillo@vub.be>
2020-08-18 19:14:53 +0200

    update content

---
+++ index.js
@@ -0,0 +1,41 @@
+
+//PORTS for all servers
+const ports = {
+    host: process.env.HOST || 3000,
+    guest1: process.env.GUEST1 || 3030,
+    guest2: process.env.GUEST2 || 3050
+}
+
+//Host server
+const expHost = require('express');
+const appHost     = expHost();
+const corsHost = require('cors')
+appHost.use(corsHost())
+const pathHost    = require('path');
+const serverHost  = require('http').createServer(appHost);
+serverHost.listen(ports.host, () => { console.log('Server listening at port %d', ports.host);});
+appHost.use(expHost.static(pathHost.join(__dirname, 'public')))
+
+
+//Guest server1
+const expGuest1 = require('express');
+const appGuest1     = expGuest1();
+const corsGuest1 = require('cors')
+appGuest1.use(corsGuest1())
+const pathGuest1    = require('path');
+const serverGuest1  = require('http').createServer(appGuest1);
+serverGuest1.listen(ports.guest1, () => { console.log('Server listening at port %d', ports.guest1);});
+appGuest1.use(expGuest1.static(pathGuest1.join(__dirname, 'public')))
+
+
+
+//Guest server2
+const expGuest2 = require('express');
+const appGuest2     = expGuest2();
+const corsGuest2 = require('cors')
+appGuest2.use(corsGuest2())
+const pathGuest2    = require('path');
+const serverGuest2  = require('http').createServer(appGuest2);
+serverGuest2.listen(ports.guest2, () => { console.log('Server listening at port %d', ports.guest2);});
+appGuest2.use(expGuest2.static(pathGuest2.join(__dirname, 'public')))
+
--- testsSuite.js
+++
@@ -1,6 +0,0 @@
-
-function chkpassword(pwd) {
-	for (var j = 0; j < 16; j++)
-		if (pwd.length == j)
-			return j;
-}
