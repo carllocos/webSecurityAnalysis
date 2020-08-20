9ccc1338180a046b6936724fbadf8f2455ddc9e4
carllocos <carlos.javier.montero.castillo@vub.be>
2020-08-20 19:59:14 +0200

    example 2 partial data leak

---
+++ public/jsflow/example2_guest.js
@@ -0,0 +1,31 @@
+loadJokeService = function (divId, imgDiv, usn) {
+
+  /**JOKE TILTE AND TEXT REQUEST**/
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onload = function () {
+    var p = JSON.parse(xhttp.responseText);
+    var el = "<h3>" + p.title + "</h3><br>";
+    el += "<p>" + p.joke + "</p>";
+    document.getElementById(divId).innerHTML = el;
+  }
+
+  xhttp.open('GET', 'http://localhost:3030/joke/?usn='+ usn);
+  xhttp.send();
+
+  /**IMAGE REQUEST **/
+  var imgReq = new XMLHttpRequest();
+  var email = lbl(document.getElementsByName('email')[0].value);
+  
+  imgReq.onload = function () {
+    document.getElementById(imgDiv).innerHTML = imgReq.responseText;
+  }
+
+  //fails when url includes sensitive data
+  var url = 'http://localhost:3030/image/joke/'
+  var invalidUrl = 'http://localhost:3030/image/joke/?email=' + email
+
+  // imgReq.open('GET', url);
+  imgReq.open('GET', invalidUrl); //fails
+  imgReq.send();
+}
--- router.js
+++ router.js
@@ -1,5 +1,7 @@
 
 const router = require('express').Router()
+const fs = require('fs');
+const imgPath = __dirname + "/public/jsflow/example2_guest_joke.html";
 
 router.get('/password/', (req, res) => {
     console.log("received password " + req.query.pswd);
@@ -8,10 +10,23 @@ router.get('/password/', (req, res) => {
 
 router.get('/joke/', (req, res) => {
     //joke reference https://www.reddit.com/r/ProgrammerHumor/comments/3rni5r/command_line_russian_roulette/
-    var title= "how programmers play russian roulette"
+    var title = "how programmers play russian roulette"
+    if(!!req.query.usn){
+        title = "Hey " + req.query.usn + " do you know how programmers play russian roulette?";
+    }
     var joke = "[ $[$RANDOM % 6]==0] && rm -rf / || echo *Click*"
     return res.json({ title: title, joke: joke })
 })
 
+
+router.get('/image/joke/', (req, res) => {
+    //joke reference https://www.reddit.com/r/ProgrammerHumor/comments/3rni5r/command_line_russian_roulette/
+    if(!!req.query.email){
+        console.log("Got your email " + req.query.email);
+    }
+
+    const img = fs.readFileSync(imgPath, { encoding: 'utf8' });
+    return res.send(img);
+})
 
 module.exports = router
