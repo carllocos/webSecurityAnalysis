0ae8872cae6a44d1cc08891d024774895d9f6b9e
carllocos <carlos.javier.montero.castillo@vub.be>
2020-08-24 21:51:08 +0200

    code refactor for img

--- public/jsflow/example2_guest.js
+++ public/jsflow/example2_guest.js
@@ -10,22 +10,21 @@ loadJokeService = function (divId, imgDiv, usn) {
     document.getElementById(divId).innerHTML = el;
   }
 
-  xhttp.open('GET', 'http://localhost:3030/joke/?usn='+ usn);
+  xhttp.open('GET', 'http://localhost:3030/joke/?usn=' + usn);
   xhttp.send();
 
   /**IMAGE REQUEST **/
   var imgReq = new XMLHttpRequest();
   var email = lbl(document.getElementsByName('email')[0].value);
-  
   imgReq.onload = function () {
     document.getElementById(imgDiv).innerHTML = imgReq.responseText;
   }
+  var invalidUrl = 'http://localhost:3030/image/joke/?email=' + email
+  imgReq.open('GET', invalidUrl); //fails
+  imgReq.send();
+
 
   //fails when url includes sensitive data
   var url = 'http://localhost:3030/image/joke/'
-  var invalidUrl = 'http://localhost:3030/image/joke/?email=' + email
-
   // imgReq.open('GET', url);
-  imgReq.open('GET', invalidUrl); //fails
-  imgReq.send();
 }
